265e641832ab0bfdd9217223b6d2105eb1ea585f
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2020-10-16 19:27:09 +0200

    move rendering script to tools

--- tools/renderer/render_examples.sh
+++ tools/renderer/render_examples.sh
@@ -1,5 +1,5 @@
 #--simple rendering script--#
-#--        v0.0.4         --#
+#--        v0.0.5         --#
 #--                       --#
 #--     programmed by     --#
 #--   Klaus Rettinghaus   --#
@@ -7,8 +7,11 @@
 echo rendering examples
 mkdir temp
 
+meidir="../../mei/v4"
+outdir="../../images/v4/ExampleImages"
+
 #-- split up examples --#
-find . -type f -name "*.mei"|while read file;
+find $meidir -type f -name "*.mei"|while read file;
 do
   saxon -ext:on -s:$file -xsl:split-mdivs.xsl
 done
@@ -19,7 +22,7 @@ do
   saxon -s:$tempfile -o:${tempfile%.mei}.ly -xsl:mei2ly.xsl include=init-guidelines.ily
   lilypond -dresolution=120 -dno-print-pages -dpreview -fpng --loglevel=BASIC_PROGRESS ${tempfile%.mei}.ly
   file=$(basename "$tempfile")
-  mv ${file%.mei}.preview.png ${file%.mei}.png
+  mv ${file%.mei}.preview.png $outdir/${file%.mei}.png
 done
 
 #-- clean up --#
